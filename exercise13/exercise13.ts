8befc05ab5c6ce8e087c07cf5820cdfc61014f79
Mohammad Ali <m.aliadnanrauf@gmail.com>
2023-07-06 19:58:27 +0500

    Completed 13 exercises

---
+++ exercise13/exercise13.ts
@@ -0,0 +1,13 @@
+const myArray = [
+    "I've always wanted to drive a Tesla Model S, especially for its electric capabilities.",
+    "I can't wait to test drive the new Audi R8 and experience its powerful V10 engine.",
+    'Owning a Lamborghini Aventador is like a dream come true for many car enthusiasts.',
+    'The Porsche 911 has a timeless design and is a symbol of precision and performance.',
+    'The Mercedes-Benz S-Class is the epitome of luxury and comfort on the road.',
+  ];
+  
+  // Print a series of statements
+  for (let i = 0; i < myArray.length; i++) {
+    console.log(`${i + 1}. ${myArray[i]}`);
+  }
+  
